73d95dbf1ce3506414af37ae9779ac52e163b0a9
packages <packages@jaredlander.com>
2011-10-02 00:00:00 +0000

    version 1.0

---
+++ R/modelInfo.r
@@ -0,0 +1,192 @@
+## information on models
+#' Model Information
+#'
+#' Extracts and builds extensive information from models
+#'
+#' Helper function for \code{\link{coefplot}}
+#' @author Jared P. Lander
+#' @seealso \code{\link{coefplot.lm}}
+#' @param model The fitted model with coefficients to be plotted
+#' @param \dots Further arguments such as shorten, only and factors
+#' @import stringr
+#' @rdname getModelInfo
+#' @return Information on the model
+#' @examples
+#'
+#' data(diamonds)
+#' model1 <- lm(price ~ carat + cut*color, data=diamonds)
+#' coefplot(model1)
+#'
+getModelInfo <- function(model, ...)
+{
+    UseMethod("getModelInfo", model)
+}
+
+
+## Builds all of the info necessary for building the graph
+# param model (lm object) the model to be plotted
+# param shorten (logical or character vector) logical if all or none of the factors should be shortened, if character then only the variables listed will be shortened
+# param factors (character vector) a list of factors to include, if NULL all of them will be included
+# param only (logical) if TRUE then only the specified factors will be computed, otherwise the included factors and their interactions will be computed
+# param \dots other options
+# return Information on the model
+#' Model Information
+#'
+#' Extracts and builds extensive information from lm and glm models
+#'
+#' Helper function for \code{\link{coefplot}}
+#' @author Jared P. Lander
+#' @seealso \code{\link{coefplot.lm}}
+#' @param model The fitted model with coefficients to be plotted
+#' @param factors Vector of factor variables that will be the only ones shown
+#' @param only logical; If factors has a value this determines how interactions are treated.  True means just that variable will be shown and not its interactions.  False means interactions will be included.
+#' @param shorten logical or character; If \code{FALSE} then coefficients for factor levels will include their variable name.  If \code{TRUE} coefficients for factor levels will be stripped of their variable names.  If a character vector of variables only coefficients for factor levels associated with those variables will the variable names stripped.
+#' @param \dots Further arguments
+#' @import stringr
+#' @rdname getModelInfo.lm
+## @method getModelInfo lm
+#' @S3method getModelInfo lm
+#' @return Information on the model
+#' @examples
+#'
+#' data(diamonds)
+#' model1 <- lm(price ~ carat + cut*color, data=diamonds)
+#' coefplot(model1)
+#'
+getModelInfo.lm <- function(model, shorten=TRUE, factors=NULL, only=NULL, ...)
+{
+    # get the model summary to easily get info out of it
+    modelSummary <- summary(model)
+    
+    ## extract coefficients and standard errors
+	coef <- modelSummary$coefficients[, 1]
+	SE <- modelSummary$coefficients[, 2]		# gets standard error from summary
+    
+    varTypes <- attr(model$terms, "dataClasses")			## These are the types of the different variables
+	factorVars <- names(varTypes[varTypes %in% c("factor", "other")])    	## The variables that are factor
+
+	# store the names of the coefficients
+    newList <- names(coef)    	## names of the coefficients
+	
+	## if there are some factor variables
+	if(length(factorVars) > 0)
+	{
+		# figure out which variable belongs to each coefficient
+		# passing just one row doesn't work
+		matchedVars <- buildFactorDF(modelFactorVars=factorVars, modelModel=model$model, modelCoefs=newList, shorten=shorten, factors=factors, only=only)
+		
+		if(!is.null(factors))
+		{
+			## since some factors are not included they must also be removed from coef's and SE's
+			remainingFactors <- which(names(coef) %in% matchedVars$Coef)
+			coef <- coef[remainingFactors]
+			SE <- SE[remainingFactors]
+			newList <- newList[remainingFactors]
+			rm(remainingFactors); gc()
+		}
+	}else
+	{
+		newList <- NA
+		matchedVars <- data.frame(Var=NA, Checkers=NA, Coef=NA, CoefShort=NA)
+	}
+    
+	rm(varTypes); gc()		# do some memory cleanup
+	
+	list(coef=coef, SE=SE, factorVars=factorVars, factorVarsHuman=factorVars, factorCoefs=newList, matchedVars=matchedVars)				## return the coefs and SEs as a named list
+}
+
+
+## can deal with this in a very simliar way to lm
+#' Model Information
+#'
+#' Extracts and builds extensive information from rxLinMod models
+#'
+#' Helper function for \code{\link{coefplot}}
+#' @author Jared P. Lander
+#' @seealso \code{\link{coefplot.lm}} \code{\link{coefplot}}
+#' @param model The fitted model with coefficients to be plotted
+#' @param factors Vector of factor variables that will be the only ones shown
+#' @param only logical; If factors has a value this determines how interactions are treated.  True means just that variable will be shown and not its interactions.  False means interactions will be included.
+#' @param shorten logical or character; If \code{FALSE} then coefficients for factor levels will include their variable name.  If \code{TRUE} coefficients for factor levels will be stripped of their variable names.  If a character vector of variables only coefficients for factor levels associated with those variables will the variable names stripped.
+#' @param \dots Further arguments
+#' @import stringr
+#' @rdname getModelInfo.rxLinMod
+## @method getModelInfo rxLinMod
+#' @S3method getModelInfo rxLinMod
+#' @return Information on the model
+#' @examples
+#'
+#' data(diamonds)
+#' model1 <- lm(price ~ carat + cut*color, data=diamonds)
+#' coefplot(model1)
+#' 
+getModelInfo.rxLinMod <- function(model, shorten=TRUE, factors=NULL, only=NULL, ...)
+{
+	# get the model summary to easily get info out of it
+    #modelSummary <- summary(model)[[1]]
+    
+    ## extract coefficients and standard errors
+	coef <- model$coefficients#modelSummary$coefficients[, 1, drop=F]             #model$coefficients          # coefficients
+	SE <- model$coef.std.error#modelSummary$coefficients[, 2, drop=F]               #model$coef.std.error		# gets standard error
+
+    ## the special characters and their escaped equivalents
+    specials <- c("!", "(", ")", "-", "=", ".")
+
+    ## if they are reducing variables, get rid of them now
+    if(!is.null(factors))
+    {
+        # take care of special characters
+        factors <- subSpecials(factors, specialChars=specials)[[1]]
+
+        # make a pipe seperated character of factors to keep
+        toKeep <- paste(factors, collapse="|")
+
+        ## if they are only keeping the factor itself, and not interactions
+        if(identical(only, TRUE))
+        {
+            # just check for when that variable is the only one
+            theString <- paste("^(", toKeep, ")=[^, ]*?$", sep="")
+        }else
+        {
+            # as long as that variable is in there
+            theString <- paste("(^|, | for )(", toKeep, ")=", sep="")
+        }
+        
+        # the ones we are going to keep
+        theKeepers <- grep(theString, rownames(coef))
+
+        # take just the coefs and SEs that were specified
+        coef <- coef[theKeepers, , drop=FALSE]
+        SE <- SE[theKeepers, , drop=FALSE]
+    }
+
+    ## figure out which variables are factors
+    # get coefficient names
+    coefNames <- rownames(coef)
+
+    # get the variables put into the formula and clean them up
+    theTerms <- paste(model$formula)[[3]]
+    theTerms <- gsub("\\*|\\+", "|", theTerms)
+    theTerms <- gsub(" ", "", theTerms)
+
+    # get those variables out of the coefficient names
+    # these are the factor variables thanks to the equal sign in the regular expression
+    factorVars <- unlist(str_extract_all(string=coefNames, pattern=paste("(^|, | for )(", theTerms, ")=", sep="")))
+
+    # strip out spaces, for, commas and equals and get unique values
+    factorVars <- unique(gsub("(^ for )|(^, )|(=$)", "", factorVars))
+
+    # build matched df
+ 
+    if(length(factorVars) > 0)
+    {
+        matchedVars <- rxVarMatcher(modelFactorVars=factorVars, modelCoefNames=coefNames, shorten=shorten)
+    }else
+    {
+		newList <- NA
+		matchedVars <- data.frame(Var=NA, Checkers=NA, Coef=NA, CoefShort=NA)
+	}
+
+ 
+    return(list(coef=coef, SE=SE, factorVars=factorVars, factorCoefs=coefNames, matchedVars=matchedVars))    			## return the coefs and SEs as a named list
+}
---
+++ R/utilities.r
@@ -0,0 +1,193 @@
+## Utilities
+## Written by Jared P. Lander
+## See LISCENSE for copyright information
+
+
+## @modelFactorVars: (character vector) names of variables that are factors
+## @modelModel: (model.matrix) the model.matrix from the model, I would like this to be changed to accept a smaller set of the model.matrix so there is less data being passed arounf
+## @modelCoefs: (character vector) the coefficient names that will be matched and shortened
+## @shorten:  (logical or character vector) if true all variables will be shortened, if false, none will be (to save computation), if a character vector, only the ones listed will be shortened, the other will remain (this may get VERY complicated
+## @factors: (character vector) a list of vectors to work with if we are only interested in a few
+## @only: (logical) if factors restricts what we are looking at then decide if we want just that variable or the stuff it interacts with too
+## have to finish dealing with only showing some factors while also shortening some, all or none
+##      should be done! Yay!
+buildFactorDF <- function(modelFactorVars, modelModel, modelCoefs, shorten=TRUE, factors=NULL, only=NULL)
+{
+    # if we are only looking for some factors, just check those saving time on the rest
+    # needs to be changed to work with exclude
+#      if(!is.null(factors))
+#      {
+#          modelFactorVars <- factors
+#      }
+    
+    # build a data.frame that matches each factor variable with it's levels
+    varDFTemp <- adply(modelFactorVars, 1, function(x, modelD) { expand.grid(x, extractLevels(x, modelD), stringsAsFactors=FALSE) }, modelModel)	## Build a frame of the variables and the coefficient names for the factor variables
+	names(varDFTemp)[2:3] <- c("Var", "Pivot")		## give good names to the frame
+
+    # match each level to every coefficient (factor or numeric)
+	varDF <- expand.grid(varDFTemp$Pivot, modelCoefs, stringsAsFactors=FALSE)
+	names(varDF)[1:2] <- c("Pivot", "Coef")		## give good names to the frame
+
+    # join the two data.frames so we have variable name, levels name and coefficient names
+    varDF <- join(varDF, varDFTemp, by="Pivot")
+
+    rm(varDFTemp); gc() # housekeeping
+	
+    ## create columns to hold altered versions of the variable and pivot, it replaces special characters with their excaped versions
+    varDF$PivotAlter <- varDF$Pivot
+	varDF$VarAlter <- varDF$Var
+	
+    ## the special characters and their escaped equivalents
+	specials <- c("!", "(", ")", "-", "=", ".")
+	
+    ## go through and do the replacing
+	alterList <- subSpecials(varDF$VarAlter, varDF$PivotAlter, specialChars=specials)
+    
+    ## put the subbed values back
+    varDF$VarAlter <- alterList[[1]]
+    varDF$PivotAlter <- alterList[[2]]
+    
+    rm(alterList); gc() # housekeeping
+    
+
+    # set up a column to keep track of which combinations are good
+	varDF$Valid<- NA
+	
+    # the short names of the coefficients
+	varDF$CoefShort <- NA
+    
+    # see if the coefficient is equivalent to the variable
+    varDF <- ddply(.data=varDF, .variables="PivotAlter", .fun=function(DF) { DF$Valid=regexpr(unique(DF$PivotAlter), DF$Coef, ignore.case=FALSE); return(DF) })
+
+    # just take the ones that match
+    varDF <- varDF[varDF$Valid > 0, ]
+    
+    varDF$VarCheck <- varDF$VarAlter
+    
+    ## if a list of variables to shorten was given, then make sure that the other variable names won't be subbed out
+    if(identical(class(shorten), "character"))      # maybe this should be class(shorten) == "character" instead to make things safer?
+    {
+        # make the ones that aren't listed "" so that they won't be subbed out
+        varDF[!varDF$Var %in% shorten, "VarAlter"] <- ""
+    }
+    
+    ## group the variable names to sub out with a "|" between each so any will be subbed out
+    ## this now creates two variables like that
+    ## Subbers is used for the coefficient shortening
+    ## Checkers is used for narrowing down the the variables
+    varDF <- ddply(varDF, .(Coef), function(vect, namer, checker, collapse, keepers) { vect$Subbers <- paste(vect[, c(namer)], collapse=collapse); vect$Checkers <- paste(vect[, c(checker)], collapse=collapse); return(vect[1, keepers]) }, namer="VarAlter", checker="VarCheck", collapse="|", keepers=c("Var", "Coef", "Subbers", "Checkers", "CoefShort"))
+
+    ## if only certain factors are to be shown, narrow down the list to them
+    if(!is.null(factors))
+    {
+    	theCheckers <- strsplit(x=varDF$Checkers, split="|", fixed=TRUE)
+	 
+    	## if they only want that variable and not it's interactions
+		if(identical(only, TRUE))
+		{
+			varDF <- varDF[varDF$Checkers %in% factors, ]
+		}else
+		{
+			## if any of the variables are in the factors to keep then keep it
+            ######################
+            ## need to adjust it so the user can specify just the interaction of factors, and not the factors themselves
+			theKeepers <- laply(theCheckers, function(x, toCheck) { any(x %in% toCheck) }, toCheck=factors)
+			varDF <- varDF[theKeepers, ]
+			rm(theKeepers); gc()
+		}
+		
+		rm(theCheckers); gc()
+    }
+    
+    # if we are not supposed to shorten the coefficients at all (shorten==FALSE) then just swap Coef into CoefShort
+    # this can be done so that the caller function just grabs Coef instead of CoefShort
+    # would be nice if this can be done higher up so not as much processing needs to be done
+    if(identical(shorten, FALSE))
+    {
+        varDF$CoefShort <- varDF$Coef
+        return(varDF[, c("Var", "Checkers", "Coef", "CoefShort")])
+    }
+    
+    # now sub out the subbers from the coef to make coef short
+    varDF <- ddply(varDF, .(Subbers), function(DF) { DF$CoefShort <- gsub(paste("(^|:)", "(", unique(DF$Subbers), ")", sep=""), "\\1", DF$Coef); return(DF) } )
+
+    # return the results
+	return(varDF[, c("Var", "Checkers", "Coef", "CoefShort")])
+}
+
+
+## get the levels of factor variables
+## @varName:  the variable to pay attention to
+## @modelModel:  the model of interest
+extractLevels <- function(varName, modelModel)
+{
+    # put the variable name in front of the level
+    paste(varName, levels(factor(modelModel[[varName]])), sep="")
+}
+
+# have to get the levels from the coef names
+# factors and only not needed because that is already taken care of previously
+## ALMOST IDENTICAL to buildFactorDF except in the way it does expand.grid
+##          would be awesome to combine the two
+rxVarMatcher <- function(modelFactorVars, modelCoefNames, modelCoefs, shorten=TRUE, factors=NULL, only=NULL)
+{
+    ## match a factor var to those that are just that var or interaction with a numeric
+    ## do it for each factor var
+    # then will find combinations
+    # put each factor var with each coefficient
+    varDF <- expand.grid(modelFactorVars, modelCoefNames, stringsAsFactors=FALSE)
+    names(varDF) <- c("Var", "Coef")
+    
+    ## check if the variable matches the coef it is paired with
+    # create columns to hold altered versions of the variable and pivot, it replaces special characters with their excaped versions
+    varDF$VarAlter <- varDF$Var
+	
+    ## the special characters and their escaped equivalents
+	specials <- c("!", "(", ")", "-", "=", ".")
+	
+    # go through and do the replacing
+	alterList <- subSpecials(varDF$VarAlter, specialChars=specials)
+    
+    # put the subbed values back
+    varDF$VarAlter <- alterList[[1]]
+    varDF$VarCheck <- varDF$VarAlter
+    varDF$CoefShort <- NA
+    
+    rm(alterList); gc() # housekeeping
+
+    # now check VarAlter against coef
+    varDF <- ddply(varDF, .variables="Var", .fun=function(DF) { DF$Valid <- regexpr(pattern=paste("(^| for |, )(", unique(DF$VarAlter), ")=", sep=""), text=DF$Coef); return(DF) })
+
+    # only keep the valid ones
+    varDF <- varDF[varDF$Valid != -1, ]
+    
+    ## if a list of variables to shorten was given, then make sure that the other variable names won't be subbed out
+    if(identical(class(shorten), "character"))      # maybe this should be class(shorten) == "character" instead to make things safer?
+    {
+        # make the ones that aren't listed "" so that they won't be subbed out
+        varDF[!varDF$Var %in% shorten, "VarAlter"] <- ""
+    }
+    
+    ## group the variable names to sub out with a "|" between each so any will be subbed out
+    ## this now creates two variables like that
+    ## Subbers is used for the coefficient shortening
+    ## Checkers is used for narrowing down the the variables
+    varDF <- ddply(varDF, .(Coef), function(vect, namer, checker, collapse, keepers) { vect$Subbers <- paste(vect[, c(namer)], collapse=collapse); vect$Checkers <- paste(vect[, c(checker)], collapse=collapse); return(vect[1, keepers]) }, namer="VarAlter", checker="VarCheck", collapse="|", keepers=c("Var", "Coef", "Subbers", "Checkers", "CoefShort"))
+    
+    # if we are not supposed to shorten the coefficients at all (shorten==FALSE) then just swap Coef into CoefShort
+    # this can be done so that the caller function just grabs Coef instead of CoefShort
+    # would be nice if this can be done higher up so not as much processing needs to be done
+    if(identical(shorten, FALSE))
+    {
+        varDF$CoefShort <- varDF$Coef
+        return(varDF[, c("Var", "Checkers", "Coef", "CoefShort")])
+    }
+
+    # do the shortening
+    varDF <- ddply(varDF, .(Subbers), function(DF) { DF$CoefShort <- gsub(paste("(^|, | for )", "(", unique(DF$Subbers), ")=", sep=""), "\\1", DF$Coef); return(DF) } )
+
+    # return the results
+    return(varDF[, c("Var", "Checkers", "Coef", "CoefShort")])
+    
+    #return(varDF)
+}
